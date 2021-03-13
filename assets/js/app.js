cc530cb008e8c33aa4bfc20abbeb62f1055816cc
Erik Englund <erik.englund1@gmail.com>
2021-03-13 20:35:55 +0100

    add on click function to dessert button

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,10 +1,9 @@
 
-
-$("#main-button").click(function menu() {
+$("#main-button").click(function () {
     let main = document.getElementById("main-button");
     main.classList.remove("btn-light");
     main.classList.add("btn-success");
-    document.getElementById("menu-cat").innerHTML = `<ol id="main-courses">
+    document.getElementById("menu-cat").innerHTML = `<ol class="menu-list">
 <li><b>Saltimbocca</b><i class="fas fa-euro-sign">14</i>
     <i class="fas fa-cart-plus"></i><br>
     Chicken Breast topped with Parma Ham, Mushrooms and White Wine Sauce
@@ -24,4 +23,34 @@ $("#main-button").click(function menu() {
 </ol>`
     let starters = document.getElementById("starters-button");
     starters.classList.remove("btn-success")
+});
+
+$("#dessert-button").click(function () {
+    let dessert = document.getElementById("dessert-button");
+    dessert.classList.remove("btn-light");
+    dessert.classList.add("btn-success");
+    document.getElementById("menu-cat").innerHTML = `<ol class="menu-list">
+    <li><b>Nutella Bella</b><i class="fas fa-euro-sign">5</i>
+        <i class="fas fa-cart-plus"></i><br>
+        Buttery Nutella Filled Dough, Topped with Strawberries
+    </li>
+    <li><b>Warm Salted Caramel Apple Tart</b><i class="fas fa-euro-sign">4</i>
+        <i class="fas fa-cart-plus"></i><br>
+        Warm Strudel Served With Vanilla Ice Cream
+    </li>
+    <li><b>Nutella Donuts</b><i class="fas fa-euro-sign">8</i>
+        <i class="fas fa-cart-plus"></i><br>
+        Four Donuts Filled With Nutella
+    </li>
+    <li><b>Key Lime Pie</b><i class="fas fa-euro-sign">9</i>
+        <i class="fas fa-cart-plus"></i><br>
+        Our Famous homemade Key Lime Pie
+    </li>
+    <li><b>Cheesecake</b><i class="fas fa-euro-sign">7</i>
+        <i class="fas fa-cart-plus"></i><br>
+        Rich and heavenly
+    </li>
+</ol>`
+    let successbutton = document.getElementById("main-button")
+    successbutton.classList.remove("btn-success")
 });
