d8c055a13fb83da4423ca20c35df84f28350e27c
Erik Englund <erik.englund1@gmail.com>
2021-03-22 15:26:40 +0100

    change margin on remove btn in cart to be responsible and work with smaller screens

--- assets/js/app.js
+++ assets/js/app.js
@@ -375,7 +375,7 @@ function addItemToCart(title, price) {
         <span class="cart-price cart-column">${price}</span>
         <div class="cart-quantity cart-column">
             <input class="cart-quantity-input" type="number" value="1">
-            <button class="btn btn-danger" type="button">REMOVE</button>
+            <button class="btn btn-danger" id="removeBtn" type="button">Remove</button>
         </div>`;
     cartRow.innerHTML = cartRowContents;
     cartItems.append(cartRow);
